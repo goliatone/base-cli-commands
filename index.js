c25dc3cab27632ed9449600ceaac12069c61c18b
goliatone <burgosemi@gmail.com>
2017-10-29 12:49:34 -0400

    export classes

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+'use strict';
+
 /**
- * Export module base-cli-commands.
- *
- * @type {Function}
+ * Export BaseCommand
  */
-module.exports = require('./lib');
+module.exports.BaseCommand = require('./lib/base');
+
+module.exports.BasePaths = require('./lib/paths');
+
+module.exports.TemplateRenderer = require('./lib/template');
+
+module.exports.FsUtils = require('./lib/fs-utils');
+
+// module.exports.BaseInstaller = require('./lib/installer');
